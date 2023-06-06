9df830bc4ab072d3579a1482fd6978f120e86172
Matheus-Pires-Gouveia <61023277+Matpires02@users.noreply.github.com>
2023-06-06 17:37:01 -0300

    Colocando a possibilidade de usar matrizes e arrays

--- src/app/entities/home/home.component.ts
+++ src/app/entities/home/home.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {equivalencePortugolStudio} from "./equivalence";
 import {FormControl, Validators} from "@angular/forms";
 
@@ -13,9 +13,10 @@ export class HomeComponent implements OnInit, AfterViewInit {
   portugolForm = new FormControl<string>(
     'programa {\n' +
     'funcao inicio() {\n' +
-    'inteiro n, fatorial\n' +
+    'inteiro n, fatorial, trid[2][4][3], array[4], matriz[3][4]\n' +
     'escreva("Entre com o valor de n: ")\n' +
     'leia(n)\n' +
+    "leia(array[3])\n" +
     'fatorial = 1\n' +
     'para (inteiro i = 1; i <= n; i = i + 1) {\n' +
     'fatorial = fatorial * i\n' +
@@ -56,12 +57,12 @@ export class HomeComponent implements OnInit, AfterViewInit {
     this.hidden = true;
     this.text = 'import java.util.Scanner;\n';
     this.variaveis = {};
-    setTimeout(()=> {
+    setTimeout(() => {
       this.replaceWords();
     }, 500)
   }
 
-  private replaceWords(){
+  private replaceWords() {
     let value: string | null = this.portugolForm.value//?.split('\n');
     // substituindo valores
     //const regex = new RegExp('(');
@@ -79,44 +80,58 @@ export class HomeComponent implements OnInit, AfterViewInit {
         spl = spl.replace(')', ' ) ');
         spl = spl.replace('{', ' {');
         spl = spl.replace('}', ' }');
-        spl = spl.replace(',', ' , ');
+        spl = spl.replace(/\,/g, ' , ');
 
-        const splitLine = spl.trim().split(' ');
-        if (splitLine.includes('leia')){
-            const vars = Object.keys(this.variaveis);
-            splitLine.forEach(word => {
-              if (vars.includes(word)){
-                // @ts-ignore
-                const type = this.variaveis[word];
-                this.setScanner(type, word);
-              }
-            });
-        } else{
+        let splitLine = spl.trim().split(' ');
+        if (splitLine.includes('leia')) {
+          const vars = Object.keys(this.variaveis);
+          splitLine.forEach(word => {
+            let wordWithoutSpace = word.replace(" ", "");
+            if (vars.includes(wordWithoutSpace)) {
+              // @ts-ignore
+              const type = this.variaveis[wordWithoutSpace];
+              this.setScanner(type, wordWithoutSpace);
+            }
+            // Verificando se a palavra é um array ou matriz
+            else if (vars.includes(word.replace(new RegExp("(\\[\\d+\\])+"), ''))) {
+              const varNameWithoutBraketsAndNumbers = word.replace(new RegExp("(\\[\\d+\\])+"), '').replace(',', '');
+              // @ts-ignore
+              const type = this.variaveis[varNameWithoutBraketsAndNumbers];
+              this.setScanner(type, word);
+            }
+          });
+        } else {
           for (let j = 0; j < splitLine.length; j++) {
-            const word = splitLine[j];
-            if (this.types.includes(word) && !splitLine.includes('para')){
-              this.setVars(spl.replace(',','').split(' '), j);
+            let word = splitLine[j];
+            if (this.types.includes(word) && !splitLine.includes('para')) {
+              let replacedArrayVars = this.setVars(spl.replace(',', '$%@#').split(' '), j);
+              splitLine = splitLine.map((value1, index) => {
+                if (index !== j && replacedArrayVars[index] != "$%@#") {
+                  return replacedArrayVars[index];
+                }
+                return value1;
+              })
             }
             // @ts-ignore
             if (this.PORTUGOL_EQUIVALENCE?.[word]) {
               // @ts-ignore
               this.text += this.PORTUGOL_EQUIVALENCE?.[word] + ' ';
             } else {
+              //console.log(word)
               this.text += word + ' ';
             }
-            if (j === splitLine.length -1){
-              console.log(splitLine)
+            if (j === splitLine.length - 1) {
+              //console.log(splitLine)
               if (word == '{' || word == '}') {
                 this.text += '\n';
-              } else if ((splitLine.length == 1 || splitLine.length==2) && (word == '' || word == ' ')){
+              } else if ((splitLine.length == 1 || splitLine.length == 2) && (word == '' || word == ' ')) {
                 this.text += '\n';
-              }
-              else{
+              } else {
                 this.text += ';\n';
               }
             }
           }
-          if (splitLine.join(' ').includes('public static void main')){
+          if (splitLine.join(' ').includes('public static void main')) {
             this.text += 'Scanner scan = new Scanner(System.in); \n';
           }
         }
@@ -127,17 +142,54 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
   private setVars(splitLine: string[], startIndex: number) {
+    const arrayRegex = new RegExp("\\w+\\[(\\d)+\\]");
+    const matrizRegex = new RegExp("\\w+(\\[\\d+\\]){2,}");
+
     const type: string = splitLine[startIndex];
-    for (let i = startIndex + 1; i < splitLine.length; i++){
-      if (splitLine[i] != '' && splitLine[i] != ' '){
-        // @ts-ignore
-        this.variaveis = {...this.variaveis, [splitLine[i]]: type }
+
+    for (let i = startIndex + 1; i < splitLine.length; i++) {
+
+      if (splitLine[i] != '' && splitLine[i] != ' ' && splitLine[i] != '$%@#') {
+
+        splitLine[i].replace(" ", '');
+        splitLine[i].replace(",", ' ,');
+        //Verificando se é uma matriz
+        if(splitLine[i].match(matrizRegex)){
+
+          let newString = splitLine[i].slice();
+          newString = newString.replace(",", ' ');
+          newString = newString.replace(" ", '');
+          newString = newString.replace(new RegExp("(\\[\\d+\\]){2,}"), '');
+          let brackets = splitLine[i].replace(new RegExp("\\w*"), '').replace(',', '');
+          while (brackets.search(new RegExp('\\d')) != -1){
+            brackets = brackets.replace(new RegExp("\\d"), '')
+          }
+          // @ts-ignore
+          splitLine[i] = newString +' '+brackets+ ' = new ' + this.getJavaType(type) + ' ' + splitLine[i].match(new RegExp("(\\[\\d+\\]){2,}"))[0] + (splitLine[i].includes(',') ? ',' : '');
+          this.variaveis = {...this.variaveis, [newString.replace(',', '')]: type};
+        }
+        //Verificando se é vetor
+        else if (splitLine[i].match(arrayRegex)) {
+          let newString = splitLine[i].slice();
+          newString = newString.replace(" ", '');
+          newString = newString.replace(",", '');
+          newString = newString.replace(new RegExp("\\[(\\d+)+\\]"), '');
+
+          // @ts-ignore
+          splitLine[i] = newString + '[] = new ' + this.getJavaType(type) + "[" + splitLine[i].match("\\d+")[0] + "]" + (splitLine[i].includes(',') ? ',' : '');
+          this.variaveis = {...this.variaveis, [newString.replace(',', '')]: type};
+        }
+        else {
+          this.variaveis = {...this.variaveis, [splitLine[i].replace(',', '')]: type};
+        }
       }
     }
+    console.log(this.variaveis)
+    return splitLine
   }
 
   private setScanner(type: string, word: string) {
-    switch (type){
+    switch (type) {
       case 'inteiro':
         this.text += word + ' = ';
         this.text += 'scan.nextInt(); \n';
@@ -152,6 +204,23 @@ export class HomeComponent implements OnInit, AfterViewInit {
         break;
       default:
         this.text += '// não é possivel ter essa entrada de dados\n';
+        break;
+    }
+  }
+
+  private getJavaType(type: string): string {
+    switch (type) {
+      case 'inteiro':
+        return "int";
+        break;
+      case 'real':
+        return "double";
+        break;
+      case 'cadeia':
+        return 'String';
+        break;
+      default:
+        return '';
         break;
     }
   }

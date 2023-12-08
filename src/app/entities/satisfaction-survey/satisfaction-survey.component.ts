3aef3c2899c0fe587e93834b6fb500fa01067a91
Matheus Pires <matpires_02@outlook.com.br>
2023-12-08 11:00:24 -0300

    Colocando pesquisa de uso e satisfação

---
+++ src/app/entities/satisfaction-survey/satisfaction-survey.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-satisfaction-survey',
+  templateUrl: './satisfaction-survey.component.html',
+  styleUrls: ['./satisfaction-survey.component.scss']
+})
+export class SatisfactionSurveyComponent {
+
+}

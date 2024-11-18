757ba8f39a73bef80b134632856bf695a6b25233
Matheus Pires <matpires_02@outlook.com.br>
2024-11-18 20:42:38 -0300

    Revert "Build v1.0"
    This reverts commit 13e2bb386e9525dd73a627b75270e814a83608b0.

--- docs/chunk-YZVWZUFN.js
+++
@@ -1,1 +0,0 @@
-import{H as n,j as m}from"./chunk-6P6P3NZN.js";import{Z as e,ha as i,rc as r}from"./chunk-BZJMNUOL.js";var s=(()=>{class o{static \u0275fac=function(a){return new(a||o)};static \u0275mod=i({type:o});static \u0275inj=e({imports:[r,m.forChild([{path:"",loadChildren:()=>import("./chunk-HIEGGR4W.js").then(t=>t.HomeModule)}]),n]})}return o})();export{s as EntitiesModule};
